104fedcfe63a8443e6bde7fd71d031803fbf6648
wekiee1 <1094216740@qq.com>
2022-11-19 00:50:10 +0800

    主应用测试提交

--- w_main/src/App.vue
+++ w_main/src/App.vue
@@ -5,23 +5,20 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ w_main/src/views/MicroAppBars.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="plan">开发计划</el-menu-item>
+    <el-menu-item index="component">组件库</el-menu-item>
+    <el-menu-item index="server">服务器</el-menu-item>
+    <el-menu-item index="waiting">待开发</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "MicroAppBars",
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
